13eb92bda780423e2c0f4734309a912713e82692
Luis <account@luishauenstein.com>
2022-03-01 20:34:14 +0100

    updates manual offset amounts

--- pages/index.js
+++ pages/index.js
@@ -48,8 +48,8 @@ async function fetchCryptoPrices() {
 }
 
 export async function getStaticProps() {
-  const ethManualOffset = 1571 /* UkraineDAO */ + 133 /* backandalive */ + 2529; /* govt */
-  const usdtManualOffset = 25000 /* backandalive */ + 1730564; /* govt */
+  const ethManualOffset = 1571 /* UkraineDAO */ + 2529; /* govt withdrawal*/
+  const usdtManualOffset = 1730564; /* govt withdrawal */
   const fiatManualOffset = 5861560; /* gavin wood 298,367 DOT donation */ // manuel fiat offset in dollars
 
   const [
